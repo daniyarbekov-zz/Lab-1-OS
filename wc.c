422644e0e508d7934ca8214989f9ddc1df870c39
Macintosh HD <dindaniyarbekov@gmail.com>
2019-01-25 16:45:24 -0500

    node creation node lookups in the hashtable using chaining

--- wc.c
+++ wc.c
@@ -34,28 +34,39 @@ struct wc *wc_init(char *word_array, long size)
 	//string parsing 
 	char *parser = word_array;
 
-	long int begIndex = 0;
-	long int  endIndex = 0;
-	int length = 0;
-	char *word;
 
+	long int begIndex, endIndex = 0;
 	unsigned long hV = 0;
 
+	char* word;
+	int count = 0;
+	int length = 0;
+	int index = 0;
 
-	while(parser + endIndex !=NULL){
-		if(!isspace((int) (parser + endIndex))){
+
+
+	while(endIndex <= size){
+		if(!isspace((int) *(parser + endIndex)) && endIndex != strlen(string)){
 			endIndex++;
-		} else {
+		} else{
 			length = (int) endIndex - begIndex;
 			if(length > 0){
 				word = (char*) malloc((length+1)*sizeof(char));
-				while(begIndex!=endIndex){
-					word[begIndex] = parser+begIndex;
+                count = 0;
+				while(count<length){
+					word[count] = *(parser+begIndex);
 					begIndex++;
+                    count++;
 				}
-				word[begIndex] = '\0';
-
-				hashValue = hV(word);
+				word[count] = '\0';
+				printf("%s",word);
+				hV = hashValue(word);
+				index = hV % SIZE;
+				if(ifPresent(word,index,wc)){
+					free(word);
+				}
+				
+				printf(" hash value is: %ld and index is: %d", hV,index);
 				endIndex++;
 				begIndex = endIndex;
 			} else{
@@ -104,9 +115,43 @@ void wc_destroy(struct wc *wc) {
 
 
 
+bool ifPresent (char* str, int index, struct wc *wc){
+	if(wc->hashMap[index] == NULL){
+		node *dummyHead = (struct node *)malloc(sizeof(struct node));
+		node *newNode = (struct node *)malloc(sizeof(struct(node)));
 
+		newNode->value = str;
+		newNode->key = index;
+		newNode->count = 1;
+		newNode->next = NULL;
+		wc->hashMap[index] = dummyHead;
+		dummyHead->next = newNode;
 
+		return false;
+	}
+	else{
+		node* pointer = wc->hashMap[index];
+		node *pointerNext = pointer->next;
+		while(pointerNext!= NULL){
+			if(strcmp(pointerNext->value, str) == 0){
+				pointerNext->count+=1;
+				return true;
+			}
+			pointer = pointerNext;
+			pointerNext = pointerNext->next;
+		}
 
+		node *pointerNext = (struct node *)malloc(sizeof(struct node));
+		pointerNext->value = str;
+		pointerNext->key = index;
+		pointerNext->count = 1;
+		pointerNext->next = NULL;
+		pointer->next = pointerNext;
+		return false;
+	}
+
+	
+}
 
 //cse.yorku.ca/~oz/html, we use 5381 as it says it works fine with it
 unsigned long hashValue (char* str){
